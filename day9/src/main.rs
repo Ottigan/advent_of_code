a35c9c487d5e088141089d68b7dbc1653b604e2a
Jānis Malcāns <jmalcans91@gmail.com>
2022-12-11 05:34:01 +0200

    ADD solution for Day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -6,7 +6,7 @@ struct Motion(char, i32);
 fn main() {
     let motions = get_motions(fs::read_to_string("src/input.txt").unwrap());
 
-    let positions_visited = get_positions_visited(motions, 2);
+    let positions_visited = get_positions_visited(motions, 10);
 
     println!("Positions visited: {positions_visited}");
 }
@@ -54,178 +54,72 @@ fn parse_motions(
     let range = 0..distance;
 
     let mut tail_path: Vec<(i32, i32)> = Vec::from([*positions.last().unwrap()]);
-    let new_positions = match direction {
-        'U' => range.fold(positions, |knots, _| {
-            let mut prev_knot: Option<(i32, i32)> = None;
-            let new_knots = knots
-                .iter()
-                .map(|knot| {
-                    let new_knot = match prev_knot {
-                        Some(prev) => {
-                            let x_diff = (prev.0 - knot.0).abs();
-                            let y_diff = (prev.1 - knot.1).abs();
-                            let adjusted_x = if prev.0 > knot.0 {
-                                prev.0 - 1
-                            } else {
-                                prev.0 + 1
-                            };
+    let new_positions = range.fold(positions, |knots, _| {
+        let mut prev_knot: Option<(i32, i32)> = None;
+        let new_knots = knots
+            .iter()
+            .map(|knot| {
+                let new_knot = match prev_knot {
+                    Some(prev) => move_knot(prev, *knot),
+                    None => match direction {
+                        'U' => (knot.0, knot.1 + 1),
+                        'R' => (knot.0 + 1, knot.1),
+                        'D' => (knot.0, knot.1 - 1),
+                        'L' => (knot.0 - 1, knot.1),
+                        _ => panic!("Such direction does not exist!"),
+                    },
+                };
 
-                            if x_diff == 2 && y_diff == 1 {
-                                (adjusted_x, prev.1)
-                            } else if x_diff == 2 && y_diff == 2 {
-                                (adjusted_x, prev.1 - 1)
-                            } else if y_diff == 2 {
-                                (prev.0, prev.1 - 1)
-                            } else {
-                                *knot
-                            }
-                        }
-                        None => (knot.0, knot.1 + 1),
-                    };
+                prev_knot = Some(new_knot);
+                new_knot
+            })
+            .collect::<Vec<(i32, i32)>>();
 
-                    prev_knot = Some(new_knot);
-                    new_knot
-                })
-                .collect::<Vec<(i32, i32)>>();
-
-            tail_path.push(*new_knots.last().unwrap());
-            new_knots
-        }),
-        'R' => range.fold(positions, |knots, _| {
-            let mut prev_knot: Option<(i32, i32)> = None;
-            let new_knots = knots
-                .iter()
-                .map(|knot| {
-                    let new_knot = match prev_knot {
-                        Some(prev) => {
-                            let x_diff = (prev.0 - knot.0).abs();
-                            let y_diff = (prev.1 - knot.1).abs();
-                            let adjusted_y = if prev.1 > knot.1 {
-                                prev.1 - 1
-                            } else {
-                                prev.1 + 1
-                            };
-                            let adjusted_x = if prev.0 > knot.0 {
-                                prev.0 - 1
-                            } else {
-                                prev.0 + 1
-                            };
-
-                            if x_diff == 2 && y_diff == 2 {
-                                (adjusted_x, adjusted_y)
-                            } else if (x_diff == 1 && y_diff == 2) || y_diff == 2 {
-                                (prev.0, adjusted_y)
-                            } else if x_diff == 2 {
-                                (prev.0 - 1, prev.1)
-                            } else {
-                                *knot
-                            }
-                        }
-                        None => (knot.0 + 1, knot.1),
-                    };
-
-                    prev_knot = Some(new_knot);
-                    new_knot
-                })
-                .collect::<Vec<(i32, i32)>>();
-
-            tail_path.push(*new_knots.last().unwrap());
-            new_knots
-        }),
-        'D' => range.fold(positions, |knots, _| {
-            let mut prev_knot: Option<(i32, i32)> = None;
-            let new_knots = knots
-                .iter()
-                .map(|knot| {
-                    let new_knot = match prev_knot {
-                        Some(prev) => {
-                            let x_diff = (prev.0 - knot.0).abs();
-                            let y_diff = (prev.1 - knot.1).abs();
-                            let adjusted_x = if prev.0 > knot.0 {
-                                prev.0 - 1
-                            } else {
-                                prev.0 + 1
-                            };
-
-                            if x_diff == 2 && y_diff == 1 {
-                                (adjusted_x, prev.1)
-                            } else if x_diff == 2 && y_diff == 2 {
-                                (adjusted_x, prev.1 + 1)
-                            } else if y_diff == 2 {
-                                (prev.0, prev.1 + 1)
-                            } else {
-                                *knot
-                            }
-                        }
-                        None => (knot.0, knot.1 - 1),
-                    };
-
-                    prev_knot = Some(new_knot);
-                    new_knot
-                })
-                .collect::<Vec<(i32, i32)>>();
-
-            tail_path.push(*new_knots.last().unwrap());
-            new_knots
-        }),
-        'L' => range.fold(positions, |knots, _| {
-            let mut prev_knot: Option<(i32, i32)> = None;
-            let new_knots = knots
-                .iter()
-                .map(|knot| {
-                    let new_knot = match prev_knot {
-                        Some(prev) => {
-                            let x_diff = (prev.0 - knot.0).abs();
-                            let y_diff = (prev.1 - knot.1).abs();
-                            let adjusted_y = if prev.1 > knot.1 {
-                                prev.1 - 1
-                            } else {
-                                prev.1 + 1
-                            };
-                            let adjusted_x = if prev.0 > knot.0 {
-                                prev.0 - 1
-                            } else {
-                                prev.0 + 1
-                            };
-
-                            if x_diff == 2 && y_diff == 2 {
-                                (adjusted_x, adjusted_y)
-                            } else if (x_diff == 1 && y_diff == 2) || y_diff == 2 {
-                                (prev.0, adjusted_y)
-                            } else if x_diff == 2 {
-                                (prev.0 + 1, prev.1)
-                            } else {
-                                *knot
-                            }
-                        }
-                        None => (knot.0 - 1, knot.1),
-                    };
-
-                    prev_knot = Some(new_knot);
-                    new_knot
-                })
-                .collect::<Vec<(i32, i32)>>();
-
-            tail_path.push(*new_knots.last().unwrap());
-            new_knots
-        }),
-        _ => panic!("Such direction does not exist!"),
-    };
+        tail_path.push(*new_knots.last().unwrap());
+        new_knots
+    });
 
     (new_positions, tail_path)
 }
 
+fn move_knot(head: (i32, i32), tail: (i32, i32)) -> (i32, i32) {
+    let x_diff = (head.0 - tail.0).abs();
+    let y_diff = (head.1 - tail.1).abs();
+    let adjusted_x = if head.0 > tail.0 {
+        head.0 - 1
+    } else {
+        head.0 + 1
+    };
+    let adjusted_y = if head.1 > tail.1 {
+        head.1 - 1
+    } else {
+        head.1 + 1
+    };
+
+    if x_diff == 2 && y_diff == 2 {
+        (adjusted_x, adjusted_y)
+    } else if x_diff == 2 {
+        (adjusted_x, head.1)
+    } else if y_diff == 2 {
+        (head.0, adjusted_y)
+    } else {
+        tail
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     use crate::{get_motions, get_positions_visited, parse_motions, Motion};
 
     #[test]
     fn parse_motions_should_work_moving_up() {
         let direction = 'U';
         let distance = 2;
-        let positions = Vec::from([(0, 0), (1, -1)]);
-        let expected_tail_path = Vec::from([(1, -1), (0, 0), (0, 1)]);
-        let expected_positions = Vec::from([(0, 2), (0, 1)]);
+        let positions = Vec::from([(-5, -5), (-6, -5)]);
+        let expected_tail_path = Vec::from([(-6, -5), (-6, -5), (-5, -4)]);
+        let expected_positions = Vec::from([(-5, -3), (-5, -4)]);
         let expected = (expected_positions, expected_tail_path);
 
         assert_eq!(parse_motions(direction, distance, positions), expected);
@@ -235,8 +129,8 @@ mod tests {
     fn parse_motions_should_work_moving_right() {
         let direction = 'R';
         let distance = 2;
-        let positions = Vec::from([(-5, -3), (-4, -3)]);
-        let expected_tail_path = Vec::from([(-4, -3), (-4, -3), (-4, -3)]);
+        let positions = Vec::from([(-5, -3), (-5, -2)]);
+        let expected_tail_path = Vec::from([(-5, -2), (-5, -2), (-4, -3)]);
         let expected_positions = Vec::from([(-3, -3), (-4, -3)]);
         let expected = (expected_positions, expected_tail_path);
 
@@ -247,9 +141,9 @@ mod tests {
     fn parse_motions_should_work_moving_down() {
         let direction = 'D';
         let distance = 2;
-        let positions = Vec::from([(-5, -3), (-4, -3)]);
-        let expected_tail_path = Vec::from([(-4, -3), (-4, -3), (-5, -4)]);
-        let expected_positions = Vec::from([(-5, -5), (-5, -4)]);
+        let positions = Vec::from([(-5, -5), (-6, -5)]);
+        let expected_tail_path = Vec::from([(-6, -5), (-6, -5), (-5, -6)]);
+        let expected_positions = Vec::from([(-5, -7), (-5, -6)]);
         let expected = (expected_positions, expected_tail_path);
 
         assert_eq!(parse_motions(direction, distance, positions), expected);
@@ -259,8 +153,8 @@ mod tests {
     fn parse_motions_should_work_moving_left() {
         let direction = 'L';
         let distance = 2;
-        let positions = Vec::from([(-5, -3), (-4, -3)]);
-        let expected_tail_path = Vec::from([(-4, -3), (-5, -3), (-6, -3)]);
+        let positions = Vec::from([(-5, -3), (-5, -2)]);
+        let expected_tail_path = Vec::from([(-5, -2), (-5, -2), (-6, -3)]);
         let expected_positions = Vec::from([(-7, -3), (-6, -3)]);
         let expected = (expected_positions, expected_tail_path);
 
@@ -311,23 +205,7 @@ mod tests {
     }
 
     #[test]
-    fn get_positions_visited_part_two_should_work() {
-        let input = Vec::from([
-            Motion('R', 4),
-            Motion('U', 4),
-            Motion('L', 3),
-            Motion('D', 1),
-            Motion('R', 4),
-            Motion('D', 1),
-            Motion('L', 5),
-            Motion('R', 2),
-        ]);
-
-        assert_eq!(get_positions_visited(input, 10), 1);
-    }
-
-    #[test]
-    fn get_positions_visited_part_two_bigger_should_work() {
+    fn get_positions_visited_part_two_sampple_should_work() {
         let input = Vec::from([
             Motion('R', 5),
             Motion('U', 8),
@@ -340,5 +218,12 @@ mod tests {
         ]);
 
         assert_eq!(get_positions_visited(input, 10), 36);
+    }
+
+    #[test]
+    fn get_positions_visited_part_two_should_work() {
+        let motions = get_motions(fs::read_to_string("src/input.txt").unwrap());
+
+        assert_eq!(get_positions_visited(motions, 10), 2793);
     }
 }
